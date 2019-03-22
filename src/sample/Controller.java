f662c8607627e7047adf51af9be62216765fe7e2
Darek <DRodzewicz@gmail.com>
2019-03-22 21:42:51 +0100

    *COMMENTS* -added light_bulb_on.png and light_bulb_off.png, when changing modes from current to Night mode changes icon between light_bulb_on.png and light_bulb_off.png - added meaningful comments

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,12 +12,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
 
@@ -78,23 +79,29 @@ public class Controller implements EventHandler<javafx.event.ActionEvent>, Initi
     @FXML private Button M_minus;
     @FXML private Button M_plus;
 
-
     //LABELS
     @FXML private Label memory_Label;
     @FXML private Label result_Label;
     @FXML private Label operator_Label;
 
+    //IMAGES
+    private Image lightbulb_on = new Image("sample/resources/light_bulb.png");
+    private Image lightbulb_off = new Image("sample/resources/light_bulb_off.png");
+
+    //IMAGEVIEW
+    @FXML private ImageView light_bulb;
     //VARIABLES
     private Calculus Calculator = new Calculus();
     private String Number = "";
 
     //STYLE SHEETS
     private String Current_StyleSheet = "sample/resources/style.css";
-    private String Previous_StyleSheet = "sample/resources/style.css";
     private String Theme_Default = "sample/resources/style.css";
     private String Theme_Dark = "sample/resources/style_2.css";
     private String Theme_Pink = "sample/resources/style_3.css";
     private String Theme_Red = "sample/resources/style_4.css";
+    private String ThemeName_temp ="";
+
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1)
@@ -103,105 +110,61 @@ public class Controller implements EventHandler<javafx.event.ActionEvent>, Initi
         Theme_choice.getItems().add("DEEP PINK");
         Theme_choice.getItems().add("FIERY RED");
         Theme_choice.getItems().add("DEFAULT");
+        Theme_choice.getSelectionModel().select("DEFAULT");
 
         //Listen Fore Selection changes
+        /*uses "change_themes" function, changes stylesheets to the one that is chosen*/
         Theme_choice.getSelectionModel().selectedItemProperty().addListener( (v,OldValue,NewValue) -> change_themes(NewValue));
     }
 
+    //SCENE EXTENDED
+    /* responsible for changing scenes from sample.xml to sample2.xml also applying all needed options like stylesheet*/
     public void ChangeScene_extended(ActionEvent event) throws IOException {
         Parent extended_calculator = FXMLLoader.load(getClass().getResource("sample2.fxml"));
         Scene scene = new Scene(extended_calculator);
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(scene);
         window.show();
+        scene.getStylesheets().clear();
         scene.getStylesheets().add(Current_StyleSheet);
     }
 
+    //SCENE STANDARD
+    /* responsible for changing scenes from sample2.xml to sample.xml also applying all needed options like stylesheet*/
     public void ChangeScene_standard(ActionEvent event) throws IOException {
         Parent extended_calculator1 = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Scene scene = new Scene(extended_calculator1);
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(scene);
         window.show();
+        scene.getStylesheets().clear();
         scene.getStylesheets().add(Current_StyleSheet);
     }
 
-    private void solve()
-    {
-        if(Calculator.getArg_1() != 0 && Calculator.getArg_2() == 0)
-        {
-            if(Number == "")
-            {
-                if (Calculator.getOperator() == "/" || Calculator.getOperator() == "*" || Calculator.getOperator() == "xpowern")
-                    Calculator.setArg_2(1);
-                else if(Calculator.getOperator() == "%")
-                    Calculator.setArg_2(Calculator.getArg_1()+1);
-                else
-                    Calculator.setArg_2(0);
-            }
-            else
-                Calculator.setArg_2(Double.parseDouble(Number));
-
-        }
-        else if(Calculator.getArg_1() == 0)
-        {
-            if(Number == "")
-                Calculator.setArg_1(0);
-            else
-                Calculator.setArg_1(Double.parseDouble(Number));
-            if (Calculator.getOperator() == "/" || Calculator.getOperator() == "*" || Calculator.getOperator() == "xpowern")
-                Calculator.setArg_2(1);
-            if(Calculator.getOperator() == "%")
-                Calculator.setArg_2(Calculator.getArg_1()+1);
-        }
-
-        Number = "";
-        Calculator.setArg_1(Calculator.calculate(Calculator.getArg_1(),Calculator.getArg_2(),Calculator.getOperator()));
-        Calculator.setArg_2(0);
-        if(Calculator.getArg_1() % 1 == 0)
-            result_Label.setText(Integer.toString((int)Calculator.getArg_1()));
-        else
-            result_Label.setText(Double.toString(Calculator.getArg_1()));
-    }
-
-
-    //REMOVE LAST CHARACTER FROM THE STRING
-    private static String removeLastChar(String str)
-    {
-        String new_stirng = "";
-        if(str.length() != 0)
-            new_stirng = str.substring(0, str.length() - 1);
-        return new_stirng;
-    }
-    //INSERT DIGIT INTO NUMEBR
-    private void insert_digit(String digit)
-    {
-        if(Number == "0")
-            Number = digit;
-        else
-            Number += digit;
-    }
-
+    //CHANGE MODES
+    /* changes mode to Night-Mode - changes stylesheet from Current to Dark Theme and vice versa*/ //*****STILL IS BUGGY - NEEDS FIXING
     @FXML
     public void change_modes(ActionEvent event)
     {
         Scene scene = anchor.getScene();
         if(Night_mode.isSelected())
         {
-            Previous_StyleSheet = Current_StyleSheet;
-            scene.getStylesheets().remove(Current_StyleSheet);
-            scene.getStylesheets().add(Theme_Dark);
-            Current_StyleSheet = Theme_Dark;
+            scene.getStylesheets().removeAll("DEEP PINK","FIERY RED");
+            Theme_choice.getSelectionModel().select("NIGHT MODE");
+            Theme_choice.getSelectionModel().selectedItemProperty().addListener( (v,OldValue,NewValue) -> ThemeName_temp = OldValue);
+            light_bulb.setImage(lightbulb_on);
         }
         else
         {
-            scene.getStylesheets().remove(Current_StyleSheet);
-            scene.getStylesheets().add(Previous_StyleSheet);
-            Current_StyleSheet = Previous_StyleSheet;
+            scene.getStylesheets().removeAll("DEEP PINK","FIERY RED");
+            Theme_choice.getSelectionModel().select(ThemeName_temp);
+            light_bulb.setImage(lightbulb_off);
         }
-
     }
 
+    //CHNAGE THEME
+    /* changes stylesheets according to what the user has chosen in the choice-box
+    * - removes a stylesheet that is currently applied on the scene and changes to the chosen one*/
     public void change_themes(String Theme_name)
     {
         Scene scene = anchor.getScene();
@@ -234,11 +197,77 @@ public class Controller implements EventHandler<javafx.event.ActionEvent>, Initi
                 scene.getStylesheets().add(Theme_Default);
                 Current_StyleSheet = Theme_Default;
             }
-
         }
-
     }
 
+    //SOLVE PROBLEM
+    /* function takes all the values from object of the class "Calculus" and resolves the problem accordingly*/
+    private void solve()
+    {
+        //if argument 1 is already set - set argument 2
+        if(Calculator.getArg_1() != 0 && Calculator.getArg_2() == 0)
+        {
+            if(Number == "")
+            {
+                if (Calculator.getOperator() == "/" || Calculator.getOperator() == "*" || Calculator.getOperator() == "xpowern")
+                    Calculator.setArg_2(1);
+                /* if operator  is % we add +1 to the argument_1 because if arg_1 = 22 then 22 % 23 = 22*/
+                else if(Calculator.getOperator() == "%")
+                    Calculator.setArg_2(Calculator.getArg_1()+1);
+                else
+                    Calculator.setArg_2(0);
+            }
+            else
+                Calculator.setArg_2(Double.parseDouble(Number));
+
+        }
+        // if argument 1 has not been set yet - do these steps
+        else if(Calculator.getArg_1() == 0)
+        {
+            /*checks if the string is empty, if yes then sets argument 1 to 0 if not then converts string to double
+            and sets that value to argument 1*/
+            if(Number == "")
+                Calculator.setArg_1(0);
+            else
+                Calculator.setArg_1(Double.parseDouble(Number));
+
+            /* cheks if the operator is div, multiply or x power n if yes then set argument 2 to one,
+            because by default argument 2 is set to 0 and "number" / 0 = inf, "number" * 0 = 0 and "number"^0 = 1*/
+            if (Calculator.getOperator() == "/" || Calculator.getOperator() == "*" || Calculator.getOperator() == "xpowern")
+                Calculator.setArg_2(1);
+
+            if(Calculator.getOperator() == "%")
+                Calculator.setArg_2(Calculator.getArg_1()+1);
+        }
+
+        Number = ""; //reset Number to "" empty string
+        Calculator.setArg_1(Calculator.calculate(Calculator.getArg_1(),Calculator.getArg_2(),Calculator.getOperator()));
+        Calculator.setArg_2(0);
+
+        /*checks if argument_1 (solution of the problem) has a decimal point if number = 22.0 then cast it to integer
+         * to display it as 22 else if number = 22.2 then display it as it is 22.2 */
+        if(Calculator.getArg_1() % 1 == 0)
+            result_Label.setText(Integer.toString((int)Calculator.getArg_1()));
+        else
+            result_Label.setText(Double.toString(Calculator.getArg_1()));
+    }
+
+    //REMOVE LAST CHARACTER FROM THE STRING
+    private static String removeLastChar(String str)
+    {
+        String new_stirng = "";
+        if(str.length() != 0)
+            new_stirng = str.substring(0, str.length() - 1);
+        return new_stirng;
+    }
+    //INSERT DIGIT INTO NUMEBR
+    private void insert_digit(String digit)
+    {
+        if(Number == "0")
+            Number = digit;
+        else
+            Number += digit;
+    }
 
     @Override
     public void handle(javafx.event.ActionEvent event) {
@@ -314,15 +343,16 @@ public class Controller implements EventHandler<javafx.event.ActionEvent>, Initi
                 Calculator.setArg_2(Double.parseDouble(Number));
             Number = "";
             Calculator.setArg_1(Calculator.calculate(Calculator.getArg_1(),Calculator.getArg_2(),Calculator.getOperator()));
-
             Calculator.setArg_2(0);
+
+            /*checks if argument_1 (solution of the problem) has a decimal point if number = 22.0 then cast it to integer
+             * to display it as 22 else if number = 22.2 then display it as it is 22.2 */
             if(Calculator.getArg_1() % 1 == 0)
                 result_Label.setText(Integer.toString((int)Calculator.getArg_1()));
             else
             {
                 result_Label.setText(Double.toString(Calculator.getArg_1()));
             }
-
         }
         if(event.getSource() == plus)
         {
@@ -330,7 +360,6 @@ public class Controller implements EventHandler<javafx.event.ActionEvent>, Initi
             solve();
             operator_Label.setText("+");
         }
-
         if(event.getSource() == minus)
         {
             Calculator.setOperator("-");
@@ -430,7 +459,6 @@ public class Controller implements EventHandler<javafx.event.ActionEvent>, Initi
 
             String hex = Integer.toHexString((int)Double.parseDouble(Number));
             result_Label.setText("0x"+hex);
-
         }
         if(event.getSource() == dec)
         {
@@ -438,7 +466,6 @@ public class Controller implements EventHandler<javafx.event.ActionEvent>, Initi
             Calculator.setArg_1((Integer.parseInt(Number)));
 
             result_Label.setText(Double.toString(Calculator.getArg_1()));
-
         }
         if(event.getSource() == oct)
         {
@@ -447,7 +474,6 @@ public class Controller implements EventHandler<javafx.event.ActionEvent>, Initi
 
             String oct = Integer.toOctalString((int)Double.parseDouble(Number));
             result_Label.setText(oct);
-
         }
         if(event.getSource() == bin)
         {
@@ -456,10 +482,7 @@ public class Controller implements EventHandler<javafx.event.ActionEvent>, Initi
 
             String bin = Integer.toBinaryString((int)Double.parseDouble(Number));
             result_Label.setText(bin);
-
         }
-
-
 
 
         //CONTROLS
@@ -471,14 +494,12 @@ public class Controller implements EventHandler<javafx.event.ActionEvent>, Initi
             operator_Label.setText("");
             Number = "0";
             result_Label.setText(Number);
-
         }
         if(event.getSource() == ce)
         {
             Number = "0";
             result_Label.setText(Number);
         }
-
         if(event.getSource() == del)
         {
             if(Number == "0")
@@ -517,7 +538,6 @@ public class Controller implements EventHandler<javafx.event.ActionEvent>, Initi
                 memory_Label.setText(Integer.toString((int)Calculator.getMArg_1()));
             else
                 memory_Label.setText(Double.toString(Calculator.getMArg_1()));
-
         }
         if(event.getSource() == M_plus)
         {
@@ -531,9 +551,7 @@ public class Controller implements EventHandler<javafx.event.ActionEvent>, Initi
                 memory_Label.setText(Integer.toString((int)Calculator.getMArg_1()));
             else
                 memory_Label.setText(Double.toString(Calculator.getMArg_1()));
-
         }
-
 
     }
 }
